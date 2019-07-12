b75c118883dface05d1fa34bccd8285f37bdb196
rihan.crh <rihan.crh@alibaba-inc.com>
2019-07-13 07:53:30 +0800

    mcf model

--- mcf_main.py
+++ mcf_main.py
@@ -19,7 +19,7 @@ from __future__ import division
 from __future__ import print_function
 
 import os
-import modeling
+import mcf_modeling
 import optimization
 import tensorflow as tf
 from tensorflow.python.client import device_lib
@@ -38,7 +38,7 @@ flags.DEFINE_string(
     "for the task.")
 
 flags.DEFINE_string(
-    "bert_config_file", None,
+    "mcf_config_file", None,
     "The config json file corresponding to the pre-trained BERT model. "
     "This specifies the model architecture.")
 
@@ -89,8 +89,8 @@ flags.DEFINE_integer("max_also_view_length", 128,
 flags.DEFINE_integer("max_items_length", 128,
                      "max_items_length")
 
-flags.DEFINE_float("label_smoothing", 0.1,
-                   "label smoothing param")
+flags.DEFINE_float("also_view_ratio", 0.1,
+                   "also_view_ratio")
 
 
 def get_available_gpus():
@@ -98,17 +98,22 @@ def get_available_gpus():
     return [x.name for x in local_device_protos if x.device_type == 'GPU']
 
 
-def file_based_input_fn_builder(input_files, is_training, batch_size):
+def file_based_input_fn_builder(user_item_files, also_view_files, is_training, batch_size):
     """Creates an `input_fn` closure to be passed to TPUEstimator."""
 
-    name_to_features = {
-        "also_view_ids": tf.VarLenFeature(tf.int64),
+    user_items_name_to_features = {
+        "user_id": tf.FixedLenFeature([1], tf.int64),
         "item_ids": tf.VarLenFeature(tf.int64),
-        "rates": tf.VarLenFeature(tf.float32),
-        "user_id": tf.FixedLenFeature([1], tf.int64)
+        "rates": tf.VarLenFeature(tf.float32)
     }
 
-    def _decode_record(record, name_to_features):
+
+    also_view_name_to_features = {
+        "item_id": tf.FixedLenFeature([1], tf.int64),
+        "also_view_ids": tf.VarLenFeature(tf.int64)
+    }
+
+    def _user_items_decode_record(record, name_to_features):
         """Decodes a record to a TensorFlow example."""
         example = tf.parse_single_example(record, name_to_features)
 
@@ -120,35 +125,56 @@ def file_based_input_fn_builder(input_files, is_training, batch_size):
                 t = tf.to_int32(t)
             example[name] = t
 
-        return example["item_ids"].values, example["rates"].values, example["also_view_ids"].values, \
-            example["user_id"][0]
+        return example["user_id"], example["item_ids"].values, example["rates"].values
+
+    def _also_view_decode_record(record, name_to_features):
+        example = tf.parse_single_example(record, name_to_features)
+
+        # tf.Example only supports tf.int64, but the TPU only supports tf.int32.
+        # So cast all int64 to int32.
+        for name in list(example.keys()):
+            t = example[name]
+            if t.dtype == tf.int64:
+                t = tf.to_int32(t)
+            example[name] = t
+
+        return example["item_id"], example["also_view_ids"].values
 
     def input_fn():
         """The actual input function."""
 
         # For training, we want a lot of parallel reading and shuffling.
         # For eval, we want no shuffling and parallel reading doesn't matter.
-        d = tf.data.TFRecordDataset(input_files)
+        user_item_dataset = tf.data.TFRecordDataset(user_item_files)
+        also_view_dataset = tf.data.TFRecordDataset(also_view_files)
+
+        d = tf.data.Dataset.zip((user_item_dataset, also_view_dataset))
+
+
         if is_training:
             d = d.repeat()
             d = d.shuffle(buffer_size=100)
 
-        d = d.map(lambda record: _decode_record(record, name_to_features))
+        d = d.map(lambda user_items, also_view: (_user_items_decode_record(user_items, user_items_name_to_features),
+                                                 _also_view_decode_record(also_view, also_view_name_to_features)))
 
-        d = d.map(lambda item_ids, rates, also_view_ids, user_id: (
-            item_ids[:FLAGS.max_items_length],
-            rates[:FLAGS.max_items_length],
-            also_view_ids[:FLAGS.max_also_view_length],
-            user_id
+
+        d = d.map(lambda user_items_feature, also_view_feature: (
+            user_items_feature[1][:FLAGS.max_items_length],
+            user_items_feature[2][:FLAGS.max_items_length],
+            also_view_feature[1][:FLAGS.max_also_view_length],
+            user_items_feature[0],
+            also_view_feature[0]
         ))
 
-        d = d.map(lambda item_ids, rates, also_view_ids, user_id: (
+        d = d.map(lambda item_ids, rates, also_view_ids, user_id, item_id: (
             item_ids,
             rates,
             also_view_ids,
             tf.ones_like(item_ids),
             tf.ones_like(also_view_ids),
-            user_id
+            user_id,
+            item_id
         ))
 
         def batching_func(x):
@@ -163,7 +189,8 @@ def file_based_input_fn_builder(input_files, is_training, batch_size):
                     tf.TensorShape([None]),  # also_view_ids
                     tf.TensorShape([None]),  # items_ids mask
                     tf.TensorShape([None]),  # also_view_mask
-                    tf.TensorShape([])),  # user_id
+                    tf.TensorShape([]), # user_id
+                    tf.TensorShape([])),  # item_id
                 # Pad the source sequences with eos tokens.
                 # (Though notice we don't generally need to do this since
                 # later on we will be masking out calculations past the true sequence.
@@ -173,17 +200,20 @@ def file_based_input_fn_builder(input_files, is_training, batch_size):
                     PAD_ID,
                     PAD_ID,
                     PAD_ID,
+                    0,
                     0))  # src_len -- unused
 
         batched_dataset = batching_func(d)
-        features = batched_dataset.map(lambda item_ids, rates, also_view_ids, items_mask, also_view_mask, user_id:
+        features = batched_dataset.map(lambda item_ids, rates, also_view_ids, items_mask,
+                                              also_view_mask, user_id, item_id:
         {
             "item_ids": item_ids,
             "rates": rates,
             "also_view_ids": also_view_ids,
             "items_mask": items_mask,
             "also_view_mask": also_view_mask,
-            "user_id": user_id
+            "user_id": user_id,
+            "item_id": item_id
         })
 
         return features
@@ -191,61 +221,48 @@ def file_based_input_fn_builder(input_files, is_training, batch_size):
     return input_fn
 
 
-def create_model(bert_config, is_training, input_ids, input_mask, segment_ids,
-                 labels, num_labels, use_one_hot_embeddings, is_prediction=False):
+def create_model(mcf_config, user_id, item_id, item_ids, also_view_ids, rates, items_mask, also_view_mask,
+                 use_one_hot_embeddings):
     """Creates a classification model."""
 
-    model = modeling.BertModel(
-        config=bert_config,
-        is_training=is_training,
-        input_ids=input_ids,
-        input_mask=input_mask,
-        token_type_ids=segment_ids,
-        use_one_hot_embeddings=use_one_hot_embeddings)
+    model = mcf_modeling.MCFModel(
+        config=mcf_config,
+        user_id=user_id,
+        item_id=item_id,
+        item_ids=item_ids,
+        also_view_ids=also_view_ids,
+        use_one_hot_embeddings=use_one_hot_embeddings,
+        scope=None)
 
     # In the demo, we are doing a simple classification task on the entire
     # segment.
     #
     # If you want to use the token-level output, use model.get_sequence_output()
     # instead.
-    output_layer = model.get_pooled_output()
-
-    hidden_size = output_layer.shape[-1].value
-
-    output_weights = tf.get_variable(
-        "output_weights", [num_labels, hidden_size],
-        initializer=tf.truncated_normal_initializer(stddev=0.02))
-
-    output_bias = tf.get_variable(
-        "output_bias", [num_labels], initializer=tf.zeros_initializer())
+    user_item_prod, also_view_prod = model.get_user_item_products()
+    with tf.variable_scope("scaled_prod"):
+        scaled_user_item_prod = tf.sigmoid(user_item_prod, name='scaled_user_item_prod')
+        scaled_also_view_prod = tf.sigmoid(also_view_prod, name='scaled_also_view_prod')
 
     with tf.variable_scope("loss"):
-        if is_training:
-            # I.e., 0.1 dropout
-            output_layer = tf.nn.dropout(output_layer, keep_prob=0.9)
+        user_item_prod_raw_loss = scaled_user_item_prod - rates
+        also_view_prod_raw_loss = scaled_also_view_prod - 1
 
-        logits = tf.matmul(output_layer, output_weights, transpose_b=True)
+        mask_user_item_loss = user_item_prod_raw_loss * items_mask
+        mask_also_view_loss = also_view_prod_raw_loss * also_view_mask
 
-        logits = tf.nn.bias_add(logits, output_bias)
-        probabilities = tf.nn.softmax(logits, axis=-1)
-        if is_prediction:
-            return tf.constant(0.0, dtype=tf.float32), tf.constant(0.0, dtype=tf.float32), logits, probabilities
-        log_probs = tf.nn.log_softmax(logits, axis=-1)
+        user_item_loss = tf.reduce_sum(mask_user_item_loss ** 2, axis=1, name='user_item_loss')
+        also_view_loss = tf.reduce_sum(mask_also_view_loss, axis=1, name='also_view_loss')
 
-        one_hot_labels = tf.one_hot(labels, depth=num_labels, dtype=tf.float32)
+        reg_losses = tf.get_collection(tf.GraphKeys.REGULARIZATION_LOSSES)
 
-        label_smoothing = tf.constant(FLAGS.label_smoothing, dtype=tf.float32)
-
-        one_hot_labels = one_hot_labels*(1 - label_smoothing) + label_smoothing / num_labels
-
-        per_example_loss = -tf.reduce_sum(one_hot_labels * log_probs, axis=-1)
-
-        loss = tf.reduce_mean(per_example_loss)
-
-        return loss, per_example_loss, logits, probabilities
+        loss = tf.reduce_mean((user_item_loss + FLAGS.also_view_ratio * also_view_loss)) + tf.reduce_mean(reg_losses)
 
 
-def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
+        return model, loss, user_item_loss, also_view_loss
+
+
+def model_fn_builder(mcf_config, init_checkpoint, learning_rate,
                      num_train_steps, num_warmup_steps, use_one_hot_embeddings):
     """Returns `model_fn` closure for TPUEstimator."""
 
@@ -256,23 +273,23 @@ def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
         for name in sorted(features.keys()):
             tf.logging.info("  name = %s, shape = %s" % (name, features[name].shape))
 
-        input_ids = features["input_ids"]
-        segment_ids = features["segment_ids"]
-        label_ids = features["label_ids"]
-        input_mask = features["input_mask"]
+        item_ids = features["item_ids"]
+        rates = features["rates"]
+        also_view_ids =features["also_view_ids"]
+        items_mask =features["items_mask"]
+        also_view_mask = features["also_view_mask"]
+        user_id = features["user_id"]
+        item_id = features["item_id"]
 
-        is_training = (mode == tf.estimator.ModeKeys.TRAIN)
-        is_prediction = (mode == tf.estimator.ModeKeys.PREDICT)
 
-        (total_loss, per_example_loss, logits, probabilities) = create_model(
-            bert_config, is_training, input_ids, input_mask, segment_ids, label_ids,
-            num_labels, use_one_hot_embeddings, is_prediction)
-
+        (model, loss, user_item_loss, also_view_loss) = create_model(mcf_config, user_id, item_id, item_ids,
+                                                                             also_view_ids, rates, items_mask,
+                                                                             also_view_mask, use_one_hot_embeddings)
         tvars = tf.trainable_variables()
         initialized_variable_names = {}
         if init_checkpoint:
             (assignment_map, initialized_variable_names
-             ) = modeling.get_assignment_map_from_checkpoint(tvars, init_checkpoint)
+             ) = mcf_modeling.get_assignment_map_from_checkpoint(tvars, init_checkpoint)
             tf.train.init_from_checkpoint(init_checkpoint, assignment_map)
 
         tf.logging.info("**** Trainable Variables ****")
@@ -286,33 +303,31 @@ def model_fn_builder(bert_config, num_labels, init_checkpoint, learning_rate,
         if mode == tf.estimator.ModeKeys.TRAIN:
 
             train_op = optimization.create_optimizer(
-                total_loss, learning_rate, num_train_steps, num_warmup_steps, False)
+                loss, learning_rate, num_train_steps, num_warmup_steps, False)
 
             output_spec = tf.estimator.EstimatorSpec(
                 mode=mode,
-                loss=total_loss,
+                loss=loss,
                 train_op=train_op,
             )
         elif mode == tf.estimator.ModeKeys.EVAL:
-            predictions = tf.argmax(logits, axis=-1, output_type=tf.int32)
-            accuracy = tf.metrics.accuracy(
-                    labels=label_ids, predictions=predictions)
-            loss = tf.metrics.mean(values=per_example_loss)
+
             eval_metrics = {
-                    "eval_accuracy": accuracy,
-                    "eval_loss": loss,
+                    "loss": loss,
+                    "user_item_loss": tf.reduce_mean(user_item_loss),
+                    "also_view_loss": tf.reduce_mean(also_view_loss)
                 }
 
             output_spec = tf.estimator.EstimatorSpec(
                 mode=mode,
-                loss=total_loss,
+                loss=loss,
                 eval_metric_ops=eval_metrics
             )
         else:
             output_spec = tf.estimator.EstimatorSpec(
                 mode=mode,
-                predictions={"probabilities": probabilities},
-                export_outputs={'predict': tf.estimator.export.PredictOutput(outputs=probabilities)}
+                predictions={"item_embedding": model.item_embedding_output,
+                             "user_embedding": model.user_embedding_output}
             )
         return output_spec
 
@@ -326,7 +341,7 @@ def main(_):
         raise ValueError(
             "At least one of `do_train`, or `do_predict' must be True.")
 
-    bert_config = modeling.BertConfig.from_json_file(FLAGS.bert_config_file)
+    mcf_config = mcf_modeling.MCFConfig.from_json_file(FLAGS.mcf_config_file)
 
     tf.gfile.MakeDirs(FLAGS.output_dir)
 
